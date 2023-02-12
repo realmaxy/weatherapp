5f01b61d48be55da39bb9451b73bbc9e10ba3420
realmaxy <max_sem777@mail.ru>
2023-02-12 10:08:07 +0300

    start prokect

---
+++ src/components/data/FetchData.js
@@ -0,0 +1,39 @@
+export let store = {
+    cityLocation: "",
+    countryLocation: "",
+    temp: 0,
+    feelsLike: 0,
+    humidity: 0,
+    weatherStatus: "",
+    pressure: 0,
+    windDir: "",
+    windSpeed: 0 
+}  
+
+export async function fetchData(city) {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=ca859fbf546341bb9d0123800231002&q=${city}&aqi=no`)
+    const data = await response.json();
+    const result = await data;
+
+
+    const {
+    current: {
+    temp_c: temp, humidity, feelslike_c: feelsLike, wind_dir: windDir, wind_mph: windSpeed,
+    condition: {text: weatherStatus}},
+    location: {
+    name: cityLocation,
+    country: countryLocation}} = result
+    
+
+    store = {
+    ...store,
+    temp,
+    humidity,
+    feelsLike,
+    windDir,
+    windSpeed,
+    weatherStatus,
+    cityLocation,
+    countryLocation,
+    }
+}
